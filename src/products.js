cc23986f715295982cd80ff0b843158017dbdf9f
521py <95997423+521py@users.noreply.github.com>
2023-02-23 14:57:21 +0700

    some start

---
+++ src/products.js
@@ -0,0 +1,39 @@
+import product1 from "./assets/1.png"
+import product2 from "./assets/2.png"
+import product3 from "./assets/3.png"
+import product4 from "./assets/4.png"
+import product5 from "./assets/5.png"
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "iPhone",
+        price: 999.0,
+        image: product1,
+    },
+    {
+        id: 2,
+        productName: "hm jeans",
+        price: 49.0,
+        image: product2,
+    },
+    {
+        id: 3,
+        productName: "headphones",
+        price: 499.0,
+        image: product3,
+    },
+    {
+        id: 4,
+        productName: "coca cola",
+        price: 0.9,
+        image: product4,
+    },
+    {
+        id: 5,
+        productName: "converse",
+        price: 99.0,
+        image: product5,
+    },
+
+];
