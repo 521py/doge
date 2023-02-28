47e5dfedaf436e9f0855d212cf92ed9f257eec96
521py <95997423+521py@users.noreply.github.com>
2023-02-28 17:50:24 +0700

    changes to second branch

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import { Link, Navigate } from 'react-router-dom';
+import { api } from '../Api/api';
+import axios from 'axios';
 
 export const SignIn = () => {
+    const onFinish = async (values) => {
+        console.log("values is...", values);
+        const responce = await axios.post("https://api.react-learning.ru/signin", values);
+        localStorage.setItem('token', responce.data.accessToken);
+        console.log("resp is", responce)
+        
+    };
+
+
+    const onFinishError = (errorInfo) => {
+        console.log("error", errorInfo);
+    };
+
     return (
-        <div className='SignIn'>SignIn page...</div>
+        <Formik
+            initialValues={{
+                email: '',
+                password: '',
+            }}
+        onSubmit={onFinish}
+        >
+            <Form>
+                <Field name="email" placeholder="email" type="email" />
+                <Field name="password" placeholder="password" type="password" />
+                <button type="submit">Submit</button>
+                <div className='SignIn'>SignIn page...</div>
+            </Form>
+        </Formik>
     )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,64 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Link } from 'react-router-dom';
-import { useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { useState } from 'react';
+import { applyMiddleware } from '@reduxjs/toolkit';
+import { api } from '../Api/api';
 
 export const SignUp = () => {
-  const [body, setBody] = useState();
+  const navigate = useNavigate();
 
-  const { isLoading, isError } = useQuery({
-    queryKey: ["signup-key"],
-    queryFn: async () => {
-      const res = await fetch("https://api.react-learning.ru/signup", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-      });
+  const { mutateAsync: requestOnReg, isLoading } = useMutation({
+    mutationFn: async (values) => {
+      const res = await api.reg(values);
 
-      const responce = await res.json();
+      const responce = await res.json()
+      console.log(responce)
 
       return responce;
     },
   });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const name = e.target[0].value;
-    const email = e.target[1].value;
-    const group = e.target[2].value;
-    const password = e.target[3].value;
-    const obj = { name, email, group, password };
-    // setBody(obj);
-  };
-
+  const handleSubmit = async (values) => {
+    console.log(values);
+    const res = await requestOnReg(values)
+    console.log(res);
+  }
 
   return (
-    <div className='SignUp'>
-      <form>
-        <input
-          type="email"
-          placeholder="email"
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+        group: '9-gr',
+      }}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <Field name="email" placeholder="email" type="email" />
+        <Field name="password" placeholder="password" type="password" />
+        <Field
+          name="group"
+          placeholder="номер группы"
         />
-        <input
-          type="password"
-          placeholder="example@example.com"
-        />
-        <input
-          type="text"
-          placeholder="9-gr"
-        />
-        <button onClick={handleSubmit}>SignUp!
-        </button>
-      </form>
-      <p>SignUp page...</p>
-      <p>Already have an account? <Link to="/signin">SignIn</Link></p>
-    </div>
-
+        <button type="submit">Submit</button>
+        <div className='SignUp'>
+          <p>SignUp page...</p>
+          <p>Already have an account? <Link to="/signin">SignIn</Link></p>
+        </div>
+      </Form>
+    </Formik>
   )
 }
-
